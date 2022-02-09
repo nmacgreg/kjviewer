55dd11e73a3dd1e5974a6bd5b63e6b5254e8ebc4
Neil MacGregor <neil.macgregor@ualberta.ca>
2022-02-09 14:01:24 -0700

    Fixed server.py, now returning the 'cars' struct, and not escaping the HTML elements for the pictures and links back to the original ads

--- server.py
+++ server.py
@@ -5,9 +5,23 @@ import json
 from json2html import *
 with open('/home/nmacgreg/.kijiji_scraper/ads.json', 'r') as myfile:
     jsondata = myfile.read()
+# parse through the data, transforming data into links
 
-# transform that into a local datastructure
-vlans = json.loads(jsondata)
+cars = json.loads(jsondata)
+
+for id, details in cars.items():
+    for key, value in details.items():
+        if key == "Url":
+           cars[id][key] = "<a href=" + value + ">" + value + "</a>" 
+        if key == "DataSource":
+           cars[id][key] = "<a href=" + value + ">" + value + "</a>" 
+
+#for counter in range(0, limit):
+    #print(cars[counter])
+    
+    #print(cars[counter]['Url'])
+    #cars[counter]['Url'] = '<a href=' + cars[counter]['Url'] + '>' + cars[counter]['Url'] + '</a>'
+
 
 
 @app.route('/')
@@ -18,7 +32,8 @@ def hello_world():
 def viewTable():
     #print(json2html.convert(json = infoFromJson))
     #return '<table><tr><td>this</td><td>that</td></tr></table>'
-    return json2html.convert(json = jsondata)
+    #return json2html.convert(json = jsondata)
+    return json2html.convert(json = cars, escape = 0)
 
 if __name__ == '__main__':
     app.run()
