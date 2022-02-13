082740e4861638844b0e4437a7637e30e5650f9e
Neil MacGregor <neil.macgregor@ualberta.ca>
2022-02-13 00:06:14 -0700

    insert.py is much improved, now actually inserting data

--- insert.py
+++ insert.py
@@ -4,10 +4,19 @@ import json
 from json2html import *
 import os
 import sqlite3
+import re
 
-conn = sqlite3.connect('cars.db')
+def removelines(value):
+    return value.replace('\n','')
+
+def removeweird(value):
+    return value.replace("\n",'')
+
+def removespaces(value):
+    return value.replace(' +','')
+
+conn = sqlite3.connect('data/cars.db')
 print("Opened database successfully");
-
  
 # Get the list of all files and directories
 path = "data/"
@@ -15,25 +24,33 @@ dir_list = os.listdir(path)
 
 days = []
 for file in dir_list:
-    with open("data/" +file, 'r') as myfile:
-        jsondata = myfile.read()
-    days.append(json.loads(jsondata))  # an array of dicts
+    if re.search(".json$", file):     # only the .json files
+        with open("data/" +file, 'r') as myfile:
+            jsondata = myfile.read()
+        days.append(json.loads(jsondata))  # an array of dicts
 
 # This does not keep track of differences beween the data, but maybe it combines the contents of multiple different files ?
+print("Inserting data from ? days worth of data", len(days))
+counter=0
+errorCounter=0
 for cars in days:
     for id, details in cars.items():
-        print("Found id=" + id + " where Title is: " + details["Title"])    
+        # Data cleaning
+        cars[id].setdefault('Title', '--No title given--') # Sometimes there is no title!
+        for key, value in details.items():
+            cars[id][key] = value.strip()           # Some fields contain a ton of whitespace
+
         try: 
-            conn.execute("INSERT INTO CARS (ID, TITLE, IMAGE, URL, DETAILS, DESCRIPTION, LOCATION, PRICE) \
-            VALUES (" + id + ", " + details["Title"] + ", " + details["Image"] + ", " +  details["Url"] + ", " + details["Details"] + ", " + details["Description"] + ", " + details["Location"] + ", " +  details["Price"] + ")")
+            conn.execute("INSERT INTO CARS (ID, TITLE, IMAGE, URL, DETAILS, DESCRIPTION, LOCATION, PRICE) VALUES ( ?, ? , ? , ? , ? , ? , ?, ?) ", \
+            (id, details["Title"], details["Image"], details["Url"], details["Details"], details["Description"], details["Location"], details["Price"]))
+            conn.commit()
+            counter += 1
         
         except sqlite3.Error as error:
             print("ID:" + id + " could not be added", error)
+            errorCounter += 1
 
-        #for key, value in details.items():
-            #if key == "Url":
-               #cars[id][key] = "<a href=" + value + ">" + value + "</a>" 
-            #if key == "DataSource":
-               #cars[id][key] = "<a href=" + value + ">" + value + "</a>" 
-
+print("Total # of records added: ", counter)
+print("Number of errors: ", errorCounter)
+conn.commit()
 conn.close()
